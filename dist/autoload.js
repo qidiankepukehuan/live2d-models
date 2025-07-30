b57543ac1603d3649316f270282edec860fd509d
qidiankepukehuan <tsblydyzbjb@163.com>
2025-07-30 10:31:55 +0800

    Update autoload.js

--- dist/autoload.js
+++ dist/autoload.js
@@ -56,7 +56,7 @@ function loadExternalResource(url, type) {
   // 配置选项的具体用法见 README.md
   initWidget({
     waifuPath: live2d_path + 'waifu-tips.json',
-    // cdnPath: 'https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/',
+    cdnPath: 'https://cdn.jsdelivr.net/gh/qidiankepukehuan/live2d-models@3c5138a604c7974605e8a96f84a743b99010b5f5/dist/',
     cubism2Path: live2d_path + 'live2d.min.js',
     cubism5Path: 'https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js',
     tools: ['hitokoto', 'asteroids', 'switch-model', 'switch-texture', 'photo', 'info', 'quit'],
