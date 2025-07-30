6b3e701b126a456cffef1d24e582d47e5c37acf9
qidiankepukehuan <tsblydyzbjb@163.com>
2025-07-30 11:07:22 +0800

    Update autoload.js

--- dist/autoload.js
+++ dist/autoload.js
@@ -5,7 +5,7 @@
 
 // Recommended to use absolute path for live2d_path parameter
 // live2d_path 参数建议使用绝对路径
-const live2d_path = 'https://cdn.jsdelivr.net/gh/qidiankepukehuan/live2d-models@1.0.1/dist/';
+const live2d_path = 'https://cdn.jsdelivr.net/gh/qidiankepukehuan/live2d-models@1.0.2/dist/';
 // const live2d_path = '/dist/';
 
 // Method to encapsulate asynchronous resource loading
